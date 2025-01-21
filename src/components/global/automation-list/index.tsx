68ddf36e2ff5c8910b9a94d69367af5336542823
sudhanshu2723 <sudhanshu2723@gmail.com>
2025-01-21 10:38:48 +0530

    dashboard added

--- src/components/global/automation-list/index.tsx
+++ src/components/global/automation-list/index.tsx
@@ -18,6 +18,7 @@ export default function AutomationList(){
     // creating optimistic updates
     // latest variable ref->Keeps the updated data
     const {latestVariable}=useMutationDataState(['create-automation'])
+    const {pathname}=usePath();
     // Updating the data to the newly updated data with a new automation
     // latestVariable will change only when new automation is created
     // and stores the new automations 
@@ -28,7 +29,7 @@ export default function AutomationList(){
         }
         return data || { data: [] }
     },[latestVariable,data])
-    const {pathname}=usePath();
+   
     console.log(data?.status)
     console.log(data)
     if(data?.status!==200 || data.data.length===0){
@@ -39,70 +40,76 @@ export default function AutomationList(){
              </div>
         )
     }
-    return (
-        <div className="flex flex-col gap-y-3">
-            {/* Mapping all the automations */}
-            {optimisticUiData.data!.map((automation)=>(
-                <Link href={`${pathname}/${automation.id}`}
-                key={automation.id}
-                className="bg-[#1D1D1D] hover:opacity-80 transition duration-100 rounded-xl p-5 border-[1px] radial--gradient--automations flex border-[#545454]"
-                >
-                <div className="flex flex-col flex-1 items-start">
-                <h2 className="text-xl font-semibold">{automation.name}</h2>
-                <p className="text-[#9B9CA0] text-sm font-light mb-2">
-                  This is from the comment
-                </p>
-                
 
-                {automation.keywords.length>0 ? 
-                (
-                    <div className="flex gap-x-2 flex-wrap mt-3">
-                    {/* Giving different colors to different type of keywords */}
-                <div className={cn(
-                            'rounded-full px-4 py-1 capitalize',
-                            (0 + 1) % 1 == 0 &&
-                              'bg-keyword-green/15 border-2 border-keyword-green',
-                            (1 + 1) % 2 == 0 &&
-                              'bg-keyword-purple/15 border-2 border-keyword-purple',
-                            (2 + 1) % 3 == 0 &&
-                              'bg-keyword-yellow/15 border-2 border-keyword-yellow',
-                            (3 + 1) % 4 == 0 &&
-                              'bg-keyword-red/15 border-2 border-keyword-red'
-                          )}
-                        >
-                          Get Started 
-                        </div>
-                        </div>):
-                (
-                <div className="rounded-full border-2 mt-3 border-dashed border-white/60 px-3 py-1">
-                    <p className="text-sm text-[#bfc0c3]">No Keywords</p>
-                  </div>
-                   )
-              }
-                        
-                  </div>
-                  <div className="flex flex-col justify-between">
-                    <p className="capitalize text-sm font-light text-[#9B9CA0]">
-                        {getMonth(automation.createdAt.getUTCMonth()+1)}{' '} 
-                        {automation.createdAt.getUTCDate() === 1
-                       ? `${automation.createdAt.getUTCDate()}st`
-                       : `${automation.createdAt.getUTCDate()}th`}{' '}
-                          {automation.createdAt.getUTCFullYear()}
-                    </p>
-                    {automation.listener?.listener ==='SMARTAI' ? 
-                     <GradientButton  type="BUTTON"
-                     className="w-full bg-background-80 text-white hover:bg-background-80">
-                         Smart AI 
-                     </GradientButton>
-                    : <Button className="bg-background-80 hover:bg-background-80 text-white">
-                    Standard
-                    </Button>
-                    }
-                   
-                    
-                  </div>
-                </Link>
-            ))}
-        </div>
+    return (
+      <div className="flex flex-col gap-y-3">
+      {optimisticUiData.data!.map((automation) => (
+        <Link
+          href={`${pathname}/${automation.id}`}
+          key={automation.id}
+          className="bg-[#1D1D1D] hover:opacity-80 transition duration-100 rounded-xl p-5 border-[1px] radial--gradient--automations flex border-[#545454]"
+        >
+          <div className="flex flex-col flex-1 items-start">
+            <h2 className="text-xl font-semibold">{automation.name}</h2>
+            <p className="text-[#9B9CA0] text-sm font-light mb-2">
+              This is from the comment
+            </p>
+        
+            {automation.keywords.length > 0 ? (
+              <div className="flex gap-x-2 flex-wrap mt-3">
+                {
+                  //@ts-ignore
+                  automation.keywords.map((keyword, key) => (
+                    <div
+                      key={keyword.id}
+                      className={cn(
+                        'rounded-full px-4 py-1 capitalize',
+                        (0 + 1) % 1 == 0 &&
+                          'bg-keyword-green/15 border-2 border-keyword-green',
+                        (1 + 1) % 2 == 0 &&
+                          'bg-keyword-purple/15 border-2 border-keyword-purple',
+                        (2 + 1) % 3 == 0 &&
+                          'bg-keyword-yellow/15 border-2 border-keyword-yellow',
+                        (3 + 1) % 4 == 0 &&
+                          'bg-keyword-red/15 border-2 border-keyword-red'
+                      )}
+                    >
+                      {keyword.word}
+                    </div>
+                  ))
+}
+              </div>
+            ) : (
+              <div className="rounded-full border-2 mt-3 border-dashed border-white/60 px-3 py-1">
+                <p className="text-sm text-[#bfc0c3]">No Keywords</p>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col justify-between">
+            <p className="capitalize text-sm font-light text-[#9B9CA0]">
+              {getMonth(automation.createdAt.getUTCMonth() + 1)}{' '}
+              {automation.createdAt.getUTCDate() === 1
+                ? `${automation.createdAt.getUTCDate()}st`
+                : `${automation.createdAt.getUTCDate()}th`}{' '}
+              {automation.createdAt.getUTCFullYear()}
+            </p>
+
+            {automation.listener?.listener === 'SMARTAI' ? (
+              <GradientButton
+                type="BUTTON"
+                className="w-full bg-background-80 text-white hover:bg-background-80"
+              >
+                Smart AI
+              </GradientButton>
+            ) : (
+              <Button className="bg-background-80 hover:bg-background-80 text-white">
+                Standard
+              </Button>
+            )}
+          </div>
+        </Link>
+      ))}
+    </div>
+
     )
 }
